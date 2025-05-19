b2cfa2172adbb3f9c41a467672ea20b804f90d1b
Jose Mateo Bernal <150805766+JoseBernal06@users.noreply.github.com>
2025-05-19 18:25:26 -0500

    pokeapi

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [CommonModule, IonicModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  pokemons: any[] = [];
+  offset = 0;
+  limit = 20;
+  loading = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadPokemons();
+  }
+
+  loadPokemons(event?: any) {
+    if (this.loading) return;
+    this.loading = true;
+
+    this.http
+      .get<any>(`https://pokeapi.co/api/v2/pokemon?offset=${this.offset}&limit=${this.limit}`)
+      .subscribe((res) => {
+        // Realiza una segunda llamada para obtener los detalles completos de cada Pokémon
+        const pokemonRequests = res.results.map((pokemon: Pokemon) => 
+          this.http.get<any>(pokemon.url).toPromise()
+        );
+
+        // Espera a que todas las solicitudes de detalles de los Pokémon se resuelvan
+        Promise.all(pokemonRequests).then((pokemonDetails) => {
+          this.pokemons = [...this.pokemons, ...pokemonDetails];
+          this.offset += this.limit;
+          this.loading = false;
+
+          if (event) {
+            event.target.complete();
+          }
+
+          // Desactiva el scroll infinito si ya no hay más Pokémon
+          if (res.next === null && event) {
+            event.target.disabled = true;
+          }
+        }).catch(() => {
+          this.loading = false;
+          if (event) {
+            event.target.complete();
+          }
+        });
+      });
+  }
+
+  getImageUrl(index: number): string {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
+import { importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(IonicModule.forRoot({})),
+    provideHttpClient(),
+    provideRouter(routes),
   ],
 });
